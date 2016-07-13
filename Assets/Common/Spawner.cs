f0562f2d9523c4442d28b689b40cc2871f79a09e
unknown <cbonde@MEL-R110-1221W.sae.au>
2016-07-13 15:48:19 +1000

    Restored

---
+++ Assets/Common/Spawner.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Spawner : MonoBehaviour
+{
+    public List<GameObject> characters;
+    public int distance;
+    public int numberOfEach;
+
+    // Use this for initialization
+	void Start ()
+    {
+	    for (int i = 0; i < numberOfEach; i++)
+	    {
+	        foreach (GameObject character in characters)
+	        {
+	            Instantiate(character, new Vector3(Random.Range(-distance, distance), 0, Random.Range(-distance, distance)),
+	                Quaternion.identity);
+	        }
+	    }
+    }
+}
